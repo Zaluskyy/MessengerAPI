4e75a070b7a07f77d26a75f90625d2632135688a
Zaluskyy <70617924+Zaluskyy@users.noreply.github.com>
2024-10-28 15:11:34 +0100

    migration changed

---
+++ Migrations/20241028140125_update.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace messenger.Migrations
+{
+    /// <inheritdoc />
+    public partial class update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Text",
+                value: "Siema");
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Text",
+                value: "Elo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ReceiverId",
+                table: "Messages",
+                column: "ReceiverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_SenderId",
+                table: "Messages",
+                column: "SenderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_ReceiverId",
+                table: "Messages",
+                column: "ReceiverId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_SenderId",
+                table: "Messages",
+                column: "SenderId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_ReceiverId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_SenderId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_ReceiverId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_SenderId",
+                table: "Messages");
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Text",
+                value: "Rucham ci mamę");
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Text",
+                value: "Twoja matka kurwa");
+        }
+    }
+}
